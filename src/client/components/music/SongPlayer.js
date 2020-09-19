8fbc032b6651c411ffa2d8f74e52a9d2716e25d5
Stanley <lims95@yahoo.com>
2020-09-19 11:47:59 -0400

    modifying spotify player

--- src/client/components/music/SongPlayer.js
+++ src/client/components/music/SongPlayer.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { getAccessToken } from '../../utils/axios';
+import React, { useEffect, useState } from 'react';
+import { getAccessToken, AxiosHttpRequest } from '../../utils/axios';
 import { useSelector, useDispatch } from 'react-redux';
 import SpotifyPlayer from 'react-spotify-web-playback';
 
@@ -7,22 +7,63 @@ import SpotifyPlayer from 'react-spotify-web-playback';
 import { getQueue, getPlayerState } from '../../store/store';
 
 const SongPlayer = () => {
-
     const songs = useSelector( ({ queue }) => queue.map(song => song.spotifyUri));
     const musicPlayer = useSelector( ({ musicPlayer }) => musicPlayer);
 
     const dispatch = useDispatch();
+
+    // TODO: PUT THIS IN REDUX STORE SO YOU CAN ACTUALLY PLAY MUSIC
+    const [ music, setMusic ] = useState({});
+
     useEffect(() => {
+        const playerCheckInterval = setInterval(() => checkForPlayer(), 1000);
+
+        const checkForPlayer = () => {
+            if (window.Spotify !== null) {
+                clearInterval(playerCheckInterval);
+                const player = new window.Spotify.Player({
+                  name: "Music Player",
+                  getOAuthToken: cb => { cb(getAccessToken()); },
+                });
+
+                setMusic(player);
+
+                createEventHandlers(player);
+                
+                // finally, connect!
+                player.connect();
+            }
+        };
+
+        const createEventHandlers = (player) => {
+            player.on('initialization_error', e => { console.error(e); });
+            player.on('account_error', e => { console.error(e); });
+            player.on('playback_error', e => { console.error(e); });
+          
+            // Playback status updates
+            player.on('player_state_changed', state => { 
+                console.log(state); 
+            });
+          
+            // Ready
+            player.on('ready', ({ device_id }) => {
+                console.log(device_id);
+            });
+        }
+
         dispatch(getPlayerState());
         console.log(musicPlayer);
     }, [ songs.length ]);
 
+    const play = () => {
+        music.togglePlay();
+    };
+
     return (
         <div className='player'>
-            <SpotifyPlayer
-                token={ getAccessToken() }
-                uris={ songs }
-            />
+            {
+                <h1 onClick={ () => play() }>Click</h1>
+            }
         </div>  
     );
 };
