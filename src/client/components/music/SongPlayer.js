45ebcaadbc54bf673c733aa9a35419f92aaf6efc
Stanley <lims95@yahoo.com>
2020-09-22 20:33:31 -0400

    music plays

--- src/client/components/music/SongPlayer.js
+++ src/client/components/music/SongPlayer.js
@@ -7,6 +7,10 @@ import SpotifyPlayer from 'react-spotify-web-playback';
 import { getQueue, getPlayerState } from '../../store/store';
 
 const SongPlayer = () => {
+    const [ device, setDevice ] = useState('');
+    const [ currentPlaying, setCurrentPlaying ] = useState({});
+    const [ paused, setPaused ] = useState(true);
+
     const songs = useSelector( ({ queue }) => queue.map(song => song.spotifyUri));
     const musicPlayer = useSelector( ({ musicPlayer }) => musicPlayer);
 
@@ -15,7 +19,14 @@ const SongPlayer = () => {
     // TODO: PUT THIS IN REDUX STORE SO YOU CAN ACTUALLY PLAY MUSIC
     const [ music, setMusic ] = useState({});
 
+    const temp = async() => {
+
+    };
+
     useEffect(() => {
+        temp();
+
+
         const playerCheckInterval = setInterval(() => checkForPlayer(), 1000);
 
         const checkForPlayer = () => {
@@ -41,28 +52,41 @@ const SongPlayer = () => {
             player.on('playback_error', e => { console.error(e); });
           
             // Playback status updates
-            player.on('player_state_changed', state => { 
-                console.log(state); 
+            player.on('player_state_changed', ({ track_window, paused }) => { 
+                const { current_track } = track_window;
+                setCurrentPlaying(current_track);
+                setPaused(paused);
             });
           
             // Ready
             player.on('ready', ({ device_id }) => {
-                console.log(device_id);
+                setDevice(device_id);
             });
         }
 
         dispatch(getPlayerState());
-        console.log(musicPlayer);
+
+        console.log(device);
     }, [ songs.length ]);
 
     const play = () => {
         music.togglePlay();
+        setPaused(!paused);
     };
 
     return (
         <div className='player'>
             {
-                <h1 onClick={ () => play() }>Click</h1>
+                currentPlaying.id && 
+                <div>
+                    <div className='currently-playing'>
+                        <img src={ currentPlaying.album.images[0].url } />
+                        <h1>{ currentPlaying.name }</h1>
+                    </div>
+                    <div className='controls'>
+                        <h1 onClick={ () => play() }>{ paused ? 'Play' : 'Pause' }</h1>
+                    </div>
+                </div>
             }
         </div>  
     );
